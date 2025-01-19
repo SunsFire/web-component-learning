2b4ae49789a754f733e57b1269245647a3cb8c06
SunsFire <Sureshamal02@gmail.com>
2025-01-19 20:22:46 +0530

    changed gradient curve with svg curve
    learn how to increase first element length and remove after time

--- Timer01/timer1.js
+++ Timer01/timer1.js
@@ -1,107 +1,102 @@
+import { generateColor } from "./gradient.js";
+/** @type {SVGElement} */
+const svg = document.getElementById("curve_timer");
+/** @type {HTMLCanvasElement} */
 const paper = document.querySelector("#paper");
 const time_label = document.querySelector(".time_sec");
-// const bg_circle = document.querySelector("#bg_circle");
-const bgArc = document.querySelector("#arcPathBg");
-const overlay = document.querySelector("#overlay_path");
-pen = paper.getContext("2d");
-const pi = Math.PI;
-let randian = pi / 180;
-paper.width = paper.clientWidth;
+/** @type {CanvasRenderingContext2D} */
+const pen = paper.getContext("2d");
+const degree = Math.PI / 180;
+const radius = paper.clientWidth * 0.8;
 paper.height = paper.clientHeight;
-const length = paper.width * 0.8;
-const small_radius = length * 0.45 - 15;
-const large_radius = length * 0.45 + 15;
-const mini_small_radius = length * 0.3 - 2;
-const mini_large_radius = length * 0.3 + 2;
-const pathM = {
-  x: paper.width * 0.5 + length * 0.45 * Math.cos(-240 * randian),
-  y: paper.height * 0.5 + length * 0.45 * Math.sin(-240 * randian),
+paper.width = paper.clientWidth;
+
+const center_points = {
+  x: paper.width * 0.5,
+  y: paper.height * 0.5,
 };
 
-const pathEnd = {
-  x: paper.width * 0.5 + length * 0.45 * Math.cos(60 * randian),
-  y: paper.height * 0.5 + length * 0.45 * Math.sin(60 * randian),
-};
-const draw = () => {
-  // TODO: find the stroke for circle (different styles like dashes and custom)
+function get_circle_point(radius, angle /* must be in degree*/) {
+  const points = {
+    x: center_points.x + radius * Math.cos(angle * degree),
+    y: center_points.y + radius * Math.sin(angle * degree),
+  };
+  return points;
+}
 
-  pen.strokeStyle = "#cdcdcd";
+function draw() {
+  pen.strokeStyle = "#a1a1a1";
   pen.lineWidth = 2;
   for (let i = -240; i <= 60; i = i + 5) {
-    const start = {
-      x: paper.width * 0.5 + mini_small_radius * Math.cos(i * randian),
-      y: paper.height * 0.5 + mini_small_radius * Math.sin(i * randian),
-    };
-
-    const end = {
-      x: paper.width * 0.5 + mini_large_radius * Math.cos(i * randian),
-      y: paper.height * 0.5 + mini_large_radius * Math.sin(i * randian),
-    };
     if (i == -180 || i == -90 || i == 0) {
-      const extra = {
-        x: paper.width * 0.5 + (mini_large_radius - 10) * Math.cos(i * randian),
-        y: paper.height * 0.5 +
-          (mini_large_radius - 10) * Math.sin(i * randian),
-      };
-      pen.beginPath();
-      pen.moveTo(extra.x, extra.y);
-      pen.lineTo(end.x, end.y);
-      pen.stroke();
+      var point1 = get_circle_point(radius * 0.3 - 9, i);
     } else {
-      pen.beginPath();
-      pen.moveTo(start.x, start.y);
-      pen.lineTo(end.x, end.y);
-      pen.stroke();
+      var point1 = get_circle_point(radius * 0.3 - 3, i);
     }
-  }
-
-  pen.lineWidth = 10;
-  pen.strokeStyle = "#f0f0f0";
-  for (let i = -240; i <= 60; i = i + 5) {
-    const start = {
-      x: paper.width * 0.5 + small_radius * Math.cos(i * randian),
-      y: paper.height * 0.5 + small_radius * Math.sin(i * randian),
-    };
-
-    const end = {
-      x: paper.width * 0.5 + large_radius * Math.cos(i * randian),
-      y: paper.height * 0.5 + large_radius * Math.sin(i * randian),
-    };
-
+    var point2 = get_circle_point(radius * 0.3 + 3, i);
     pen.beginPath();
-    pen.moveTo(start.x, start.y);
-    pen.lineTo(end.x, end.y);
+    pen.moveTo(point1.x, point1.y);
+    pen.lineTo(point2.x, point2.y);
     pen.stroke();
   }
-};
+}
+
 draw();
 
+function setAttributes(element, attrs) {
+  for (const [key, value] of Object.entries(attrs)) {
+    element.setAttribute(key, value);
+  }
+}
+function curve_no_animation() {
+  for (let i = -240; i < 60; i = i + 5) {
+    const point1 = get_circle_point(radius * 0.45 + 15, i);
+    const point2 = get_circle_point(radius * 0.45 - 15, i);
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    setAttributes(line, {
+      x1: point1.x,
+      y1: point1.y,
+      x2: point2.x,
+      y2: point2.y,
+      stroke: "#a1a1a1",
+      "stroke-width": "5",
+      "stroke-linecap": "round",
+    });
+    svg.appendChild(line);
+  }
+}
+
+curve_no_animation();
+const totalLines = 60;
+let colors = generateColor("#de1d1d", "#524A83", 60);
+function curve_animation(n) {
+  for (let i = 0; i < n; i++) {
+    const currentAngle = -240 + i * (300 / totalLines);
+    const point1 = get_circle_point(radius * 0.45 + 20, currentAngle);
+    const point2 = get_circle_point(radius * 0.45 - 20, currentAngle);
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    setAttributes(line, {
+      x1: point1.x,
+      y1: point1.y,
+      x2: point2.x,
+      y2: point2.y,
+      stroke: "#" + colors[i],
+      "stroke-width": "5",
+      "stroke-linecap": "round",
+      id: "a1",
+    });
+    svg.appendChild(line);
+  }
+}
+
 let time = 0;
-overlay.setAttribute(
-  "d",
-  `M ${pathM.x} ${pathM.y} A ${length * 0.45} ${length * 0.45
-  } 0 1 1 ${pathEnd.x} ${pathEnd.y}`,
-);
-
 setInterval(() => {
-  time_label.textContent = time;
-
-  const pathEnd = {
-    x: paper.width * 0.5 +
-      length * 0.45 * Math.cos((10 * time - 240) * randian),
-    y: paper.height * 0.5 +
-      length * 0.45 * Math.sin((10 * time - 240) * randian),
-  };
-
-  const largeArcFlag = 10 * time - 240 < -64 ? 0 : 1;
-
-  const newPath = `M ${pathM.x} ${pathM.y} A ${length * 0.45} ${length * 0.45
-    } 0 ${largeArcFlag} 1 ${pathEnd.x} ${pathEnd.y}`;
-
-  bgArc.setAttribute("d", newPath);
-
   time++;
+  curve_animation(time * 2);
   if (time == 31) {
     time = 0;
+    const elements = document.querySelectorAll("#a1");
+    elements.forEach((element) => element.remove());
   }
-}, 1000);
+  time_label.textContent = time;
+}, 200);
