36a1a8b180dcb341ee2d0a99fdf7ac85c6f0e815
SunsFire <Sureshamal02@gmail.com>
2025-01-23 21:51:44 +0530

    completed TopNavBar
    Completed TopNavBar but animations are not good or i don't know how to
    do that type of animation

--- TopNavbar01/topnavbar.js
+++ TopNavbar01/topnavbar.js
@@ -4,9 +4,10 @@ const serviceContents = [
 	document.getElementById("ServiveContent1"),
 	document.getElementById("ServiveContent2"),
 	document.getElementById("ServiveContent3"),
-	document.getElementById("ServiveContent4"),
 ];
 const nav = document.getElementById("nav");
+const navPoints = nav.getBoundingClientRect();
+let currentIndex = -1;
 
 navEle.forEach((item, index) => {
 	item.addEventListener("mouseover", () => {
@@ -20,18 +21,26 @@ navEle.forEach((item, index) => {
 		hoverDisplay.style.display = "block";
 		serviceContents[index].style.display = "block";
 		hoverDisplay.appendChild(serviceContents[index]);
-		const ele_points = serviceContents[index].getBoundingClientRect();
 		const navPoint = item.getBoundingClientRect();
 		const hoverPoint = hoverDisplay.getBoundingClientRect();
 		const navCenter = navPoint.left + navPoint.width / 2;
 		hoverDisplay.style.left = `${navCenter - hoverPoint.width / 2}px`;
-	});
-
-	item.addEventListener("mouseout", () => {
-		serviceContents[index].style.display = "none";
+		currentIndex = index;
 	});
 });
 
-nav.addEventListener("mouseout", () => {
-	hoverDisplay.style.display = "none";
+hoverDisplay.addEventListener("mouseleave", () => {
+	hideHoverDisplay();
 });
+
+nav.addEventListener("mouseleave", () => {
+	hideHoverDisplay();
+});
+
+function hideHoverDisplay() {
+	if (currentIndex !== -1) {
+		serviceContents[currentIndex].style.display = "none";
+		hoverDisplay.style.display = "none";
+		currentIndex = -1; // Reset the active index
+	}
+}
